2ab73e88cd5420c73ac237979223543cf0cce19a
NiallSom <niallsomers205@gmail.com>
2025-03-06 12:02:47 +0000

    Base constructor calls other time constructor with base time of 5 `this(5);`,

--- src/simulation/BasicPerson.java
+++ src/simulation/BasicPerson.java
@@ -3,11 +3,12 @@ package simulation;
 // Concrete implementation of Person
 class BasicPerson implements Person {
     private int time;
-
     public BasicPerson(int time) {
         this.time = time;
     } // to maybe add variance to different type of person i.e. elderly
-    public BasicPerson() {}
+    public BasicPerson() {
+        this(5);
+    }
     @Override
     public int getTime() {
         return time;
