158110da3a54b8957d1f39b050d98253116f5568
fred <frederickjssheppard@gmail.com>
2025-03-15 22:25:03 +0000

    Add toStrings to strategy classes

--- src/simulation/RandomStrategy.java
+++ src/simulation/RandomStrategy.java
@@ -6,8 +6,13 @@ import java.util.Random;
 
 public class RandomStrategy implements Strategy {
     private final Random random = new Random();
+
     @Override
     public void sortPassengers(ArrayList<Person> passengers) {
         Collections.shuffle(passengers, random);
+    }
+
+    public String toString() {
+        return "RandomStrategy";
     }
 }
